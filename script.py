b2cc4bdd9e55670ad0aac553f9ea2839f98a8418
Taras Strikha <yaagresor@gmail.com>
2021-11-29 16:18:01 +0200

    Додав файл з роботою по пітону

---
+++ script.py
@@ -0,0 +1,35 @@
+name="THROLL"
+greeting=name
+print("3,2,1")
+print("GO!!!!")
+print("Taras Strikha!!!!")
+print (greeting)
+followes= "999"
+print("Players of the HORDE:"+ followes)
+Alians="Alians suck hord's dicks :"+"1000"
+print(Alians)
+print("You killed:")
+number_of_kills=40
+print(number_of_kills+1)
+killed_dd= 24
+killed_rdd= 16
+total=(killed_rdd+killed_dd)
+print("Total kills:")
+print(total)
+correct=True
+print(correct)
+status= False
+print( not status)
+orc=True
+human=not orc
+print(human) 
+print(10==10)
+print(9==10)
+print( not killed_rdd==10)
+print(10!=10)
+result= 1!=2
+print(result)
+one= 1
+two= 2
+print(one == two)
+print(one != two)
